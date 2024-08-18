311baef2f3022e9b15a96d5f1422c78051b36abe
Jolomolokolo <93781011+Jolomolokolo@users.noreply.github.com>
2024-08-18 22:03:03 +0200

    Made simple FizzBuzz App

--- Project-1/main.js
+++ Project-1/main.js
@@ -1,6 +1,15 @@
 let i = 0;
 
 while (i <= 100) {
-    console.log(i);
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    else if (i % 7 == 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+    
     i++;
 }
