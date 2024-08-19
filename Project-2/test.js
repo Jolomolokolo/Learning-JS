10f6e29661fb2387c324658efe57364780c93bd0
Jolomolokolo <93781011+Jolomolokolo@users.noreply.github.com>
2024-08-19 20:33:36 +0200

    Added checking for fake numbers

--- Project-2/test.js
+++ Project-2/test.js
@@ -2,10 +2,18 @@ secret = Math.floor(Math.random() * 100) + 1;
 console.log(secret);
 
 let attempts = 0;
+let guess = undefined;
+let fail = 0;
 
 while (attempts < 5 ) {
-    let guess = parseInt(prompt("Guess the number between 1 and 100: "));
+
+     let guess = parseInt(prompt("Guess the number between 1 and 100: "));
+    if (isNaN(guess) || guess < 1 || guess > 100) {
+        console.log("Invalid input. Please enter a number between 1 and 100.");
+        continue;
+    }
     console.log(guess);
+    
     if (guess == secret) {
         console.log("Congratulations! You guessed the number!");
         break;
